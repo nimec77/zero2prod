d92b782b9dca6ea59fec6e9beb86ae03fb473e25
Dmitry Seloustev <d.seloustev@adguard.com>
2025-04-19 10:20:34 +0700

    3.6 Refocus

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let server = run()?;
-    server.await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,17 +1,22 @@
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
-    let handle = tokio::spawn(server);
-    std::mem::drop(handle);
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    tokio::spawn(server);
+
+    format!("http://127.0.0.1:{port}")
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{address}/health_check"))
         .send()
         .await
         .expect("Failed to execute request.");
