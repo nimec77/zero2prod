6e5bd6c181a17b67cc23abcb4111012090975383
Dmitry Seloustev <d.seloustev@adguard.com>
2025-04-28 22:20:43 +0700

    4.5.13 Protect Your Secrets - secrecy

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,49 +1,51 @@
-use actix_web::{HttpResponse, Responder, web};
+use actix_web::{HttpResponse, web};
 use serde::Deserialize;
 use sqlx::{PgPool, types::chrono::Utc};
-use tracing::Instrument;
 use uuid::Uuid;
+
 #[derive(Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
 }
 
-pub async fn subscriptions(
-    form: web::Form<FormData>,
-    connection: web::Data<PgPool>,
-) -> impl Responder {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-    "Adding a new subscriber.",
-    %request_id,
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),    
+    fields(
+    request_id = %Uuid::new_v4(),
     subscriber_email = %form.email,
     subscriber_name= %form.name
-    );
-    let _request_span = request_span.enter();
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    match sqlx::query!(
+    )
+)]
+pub async fn subscriptions(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&form, &pool).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
-        "#,
-        uuid::Uuid::new_v4(),
+            INSERT INTO subscriptions (id, email, name, subscribed_at)
+            VALUES ($1, $2, $3, $4)
+            "#,
+        Uuid::new_v4(),
         form.email,
         form.name,
         Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`.
-    .execute(connection.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
-    {
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("Failed to execute query: {e:?}");
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {e:?}");
+        e
+    })?;
+
+    Ok(())
 }
