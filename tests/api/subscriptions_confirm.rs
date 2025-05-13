fd42b8d48be9b6526425f059c1e9efd730d28aac
Dmitry Seloustev <d.seloustev@adguard.com>
2025-05-14 06:37:23 +0700

    Revert test: 'the_link_returned_by_subscribe_returns_a_200_if_called'

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -38,6 +38,33 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
     let confirmation_links = test_app.get_confirmation_links(email_request);
 
     // Act
+    let response = reqwest::get(confirmation_links.html)
+        .await
+        .expect("Failed to send GET request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let name = fake_name();
+    let email = fake_email().as_ref().to_owned();
+    let body = format!("name={name}&email={email}");
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    test_app.post_subscriptions(body).await;
+    let email_request = &test_app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = test_app.get_confirmation_links(email_request);
+
+    // Act
     reqwest::get(confirmation_links.html)
         .await
         .unwrap()
