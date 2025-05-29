06e6a5f9e71e30bced66926ce95bf8fc84e95ac3
Dmitry Seloustev <d.seloustev@adguard.com>
2025-05-29 21:38:02 +0700

    9.7 Send Newsletter Emails

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -7,3 +7,16 @@ pub use health_check::health_check;
 pub use newsletter::publish_newsletter;
 pub use subscriptions::subscribe;
 pub use subscriptions_confirm::confirm;
+
+pub fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{e}\n")?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Caused by:\n\t{cause}")?;
+        current = cause.source();
+    }
+    Ok(())
+}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,18 +11,7 @@ use crate::{
     startup::ApplicationBaseUrl,
 };
 
-fn error_chain_fmt(
-    e: &impl std::error::Error,
-    f: &mut std::fmt::Formatter<'_>,
-) -> std::fmt::Result {
-    writeln!(f, "{e}\n")?;
-    let mut current = e.source();
-    while let Some(cause) = current {
-        writeln!(f, "Caused by:\n\t{cause}")?;
-        current = cause.source();
-    }
-    Ok(())
-}
+use super::error_chain_fmt;
 
 pub struct StoreTokenError(sqlx::Error);
 
